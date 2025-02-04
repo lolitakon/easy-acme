5a4b5dc8b78684958d341cb43543ce9ff171ff26
lolitakon <105233192+lolitakon@users.noreply.github.com>
2025-02-04 12:57:16 +0800

    Update easy_acme.sh

--- easy_acme.sh
+++ easy_acme.sh
@@ -1,6 +1,6 @@
 # 判断acme.sh是否已安装
 acme_path="/root/.acme.sh/acme.sh"
-if [ -e $acme_path ] then;
+if [ -e $acme_path ]; then
   echo "acme.sh已安装，跳过安装"
 else
   echo "acme.sh未安装，正在安装"
@@ -13,11 +13,11 @@ fi
 # acme.sh申请证书
 read -p "请选择工作模式（ch（change）/new）" work_mode
 read -p "请输入证书存放路径（默认/opt/tls/）：" CA_Path
-if [ $CA_Path = "" ] then;
+if [ $CA_Path = "" ]; then
   CA_Path="/opt/tls/"
 fi
 
-if [ $work_mode = "ch" ] then;
+if [ $work_mode = "ch" ]; then
   read -p "注意！此模式会先清空您目标路径$CA_Path下的所有文件，如需退出请按ctrl+c"
   rm -rf $CA_Path"/*"
 else
@@ -28,20 +28,20 @@ systemctl stop nginx
 echo "acme.sh证书申请"
 read -p "请输入你的域名：" domainName
 read -p "是否使用IPV6申请?(y/n（默认）)："  isIpv6
-  read -p "请选择申请模式（standalone(s 默认)/nginx(n)）：" mode
-  if [ $mode = "n" ]; then 
-  mode="nginx"
+read -p "请选择申请模式（standalone(s 默认)/nginx(n)）：" mode
+if [ $mode = "n" ]; then 
+	mode="nginx"
 else
-  mode="standalone"
+	mode="standalone"
 fi
 
 acme --set-default-ca --server letsencrypt
 
-  if [ $isIpv6 = "y" ]; then 
-  acme  --issue -d $domainName --$mode -k ec-256 --listen-v6
+if [ $isIpv6 = "y" ]; then 
+	acme  --issue -d $domainName --$mode -k ec-256 --listen-v6
 else
-  acme  --issue -d $domainName --$mode -k ec-256
+	acme  --issue -d $domainName --$mode -k ec-256
 fi
-  
+
 acme --installcert -d $domainName --ecc  --key-file   /opt/tls/server.key   --fullchain-file /opt/tls/server.crt
 
